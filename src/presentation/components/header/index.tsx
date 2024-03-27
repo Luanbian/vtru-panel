b4b26033aab5a36dac50677e75abbd853b58e2fa
Luan Almeida <luan.almeida@jbtec.com.br>
2024-03-27 12:02:58 -0300

    feat(header): apply css

--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 export default function Header(): React.JSX.Element {
     return (
-        <>
+        <main className={styles.main}>
             <a href="/">
                 https://store.vitruveo.xyz/preview/artworkID/seo-title
             </a>
             <a href="/">
                 https://store.vitruveo.xyz/creator-name/artworkID/seo-title
             </a>
-        </>
+        </main>
     );
 }
